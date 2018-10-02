ae8bf16842de58f63028fede1df93c897be4bfe5
Tomas Toledo <trtoledo1977@gmail.com>
2018-10-02 03:28:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password])
 		#your code here!
 	end
 
